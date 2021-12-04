84409f4810496069c2937121f8c05720fbecf572
Admin <admin@MacBook-Air-Admin.local>
2021-12-04 19:41:13 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector('.infoHTML');
+let text = document.querySelector('.information');
+
+btn.addEventListener('click', () =>{
+    btn.style.display = 'none';
+    text.style.display = 'block';
+});
+
+//примеры 
+//

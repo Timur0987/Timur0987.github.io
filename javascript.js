fb91453d391714366d48dd86a01a19c6159693db
tim <tima.trigubov@icloud.com>
2021-12-06 12:20:33 +0300

    Анимация текста

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ let text = document.querySelector('.information');
 btn.addEventListener('click', () =>{
     btn.style.display = 'none';
     text.style.display = 'block';
+    btn.classList.add('animation-hide');
+    text.classList.add('animation-show');
 });
 
 //примеры 
